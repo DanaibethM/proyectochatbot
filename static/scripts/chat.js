fe10828930fb5b59e162dc8a812235fc898b8199
Valeriasilva21 <valeria2110.vs@gmail.com>
2021-12-13 16:43:45 -0400

    message

---
+++ static/scripts/chat.js
@@ -0,0 +1,105 @@
+// pestaña plegable
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        let content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    });
+}
+
+//Obtener la hora actual 
+function getTime() {
+    let today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let time = hours + ":" + minutes;
+    return time;
+}
+
+// Crear el primer mensaje
+function firstBotMessage() {
+    let firstMessage = "Hola, estoy para ayudarte. Por favor hasme una pregunta"
+    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+
+    let time = getTime();
+
+    $("#chat-timestamp").append(time);
+    document.getElementById("userInput").scrollIntoView(false);
+}
+
+firstBotMessage();
+
+function getHardResponse(userText) {
+    let botResponse = getBotResponse(userText);
+    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+    $("#chatbox").append(botHtml);
+
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+
+//Obtener la respuesta 
+function getResponse() {
+    let userText = $("#textInput").val();
+
+    if (userText == "") {
+        userText = "Hola";
+    }
+
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    setTimeout(() => {
+        getHardResponse(userText);
+    }, 1000)
+
+}
+
+
+// enviar texto 
+function buttonSendText(sampleText) {
+    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    
+    
+}
+
+function sendButton() {
+    getResponse();
+}
+
+function likeButton() {
+    buttonSendText("Me gusto poder ayudar!")
+}
+
+// Pulsa enter para enviar un mensaje
+$("#textInput").keypress(function (e) {
+    if (e.which == 13) {
+        getResponse();
+    }
+});
---
+++ static/scripts/responses.js
@@ -0,0 +1,14 @@
+function getBotResponse(input) {
+
+    if (input == "Hola") {
+        return "Hola, en que puedo ayudarte?";
+    } else if (input == "Cuando comienzan las clases") {
+        return "El inicio a clases? 6 de enero de 2022";
+    } else if (input == "Gracias, chao") {
+        return "¡Hablaré contigo más tarde!";
+    } else if (input == "Gracias") {
+        return "¡Hablaré contigo más tarde!";
+    } else {
+        return "¡Intenta preguntar algo más!";
+    }
+}
